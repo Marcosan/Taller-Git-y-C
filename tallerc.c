5cf128b07f1a5aafd0120bd1fb85fcf0059a9e65
marcosan <marcoxavibsc@gmail.com>
2017-05-18 16:21:44 -0500

    [UPD] Respaldo al finalizar la clase

--- tallerc.c
+++ tallerc.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
 
-
+void cifrado(char frase[]);
 
 
 int main(int argc, char **argv){
-
+	char cadena[100];
+	printf("Ingrese frase: ");
+	fgets(cadena, 100, stdin);
+	printf("cadena es: %s", cadena);
 
 	return 0;
+}
+
+
+void cifrado(char frase[]){
+
 }
