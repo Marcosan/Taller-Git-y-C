ef66a5b36df5d211ae4103f75217ace0f1e12789
marcosan <marcoxavibsc@gmail.com>
2017-06-04 18:54:51 -0500

    [FIX] Cifrado no interactivo (cadena , llave)

--- tallerc.c
+++ tallerc.c
@@ -9,17 +9,27 @@ int main(int argc, char **argv){
 	char cadena[100];
 	int llave;
 
-	printf("%s + %c\n", argv[1], argv[2][0]);
+	//printf("%d + %d\n", argv[1], argv[2]);
 	
-	printf("Ingrese frase: ");
-	fgets(cadena, 100, stdin);
 	
-	printf("Ingrese llave: ");
-	//llave = getchar() - '0';
-	scanf("%d",&llave);
     
-	//imprimirCifrado(argv[1], argv[2][0] - '0');
-	imprimirCifrado(cadena, llave);
+	if ( (int)argv[1] == 0 || (int)argv[2] == 0 )
+	{
+		printf("Interactivo:\n");
+		printf("Ingrese frase: ");
+		fgets(cadena, 100, stdin);
+		
+		printf("Ingrese llave: ");
+		//llave = getchar() - '0';
+		scanf("%d",&llave);
+		imprimirCifrado(cadena, llave);
+		//
+	} else {
+		printf("No Interactivo:\n");
+		imprimirCifrado(argv[1], argv[2][0] - '0');
+	}
+	
+	
 	
 	return 0;
 }
@@ -27,6 +37,7 @@ int main(int argc, char **argv){
 
 void imprimirCifrado(char cadena[], int llave){
 	int new_cadena[100];
+	printf("Mensaje Cifrado: ");
 	
 	for(int i=0;i<=strlen(cadena);i++){
 		if ((cadena[i] >= 65 && cadena[i] <= 90) || (cadena[i] >= 97 && cadena[i] <= 122) ){
